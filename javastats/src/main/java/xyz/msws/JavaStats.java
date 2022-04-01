37f3000929d38f79e1945b4d44079919446981aa
MSWS <imodmaker@gmail.com>
2022-04-01 06:10:41 -0700

    Defaults / 8080

--- javastats/src/main/java/xyz/msws/JavaStats.java
+++ javastats/src/main/java/xyz/msws/JavaStats.java
@@ -31,7 +31,6 @@ import xyz.msws.server.StatConfig;
 @Controller
 @SpringBootApplication
 public class JavaStats {
-
     @RequestMapping("/")
     @ResponseBody
     String home() {
@@ -48,67 +47,71 @@ public class JavaStats {
 // @RestController
 // @Controller
 // public class JavaStats extends TimerTask {
-//     private Map<String, ServerData> servers = new HashMap<>();
-//     private List<ServerConfig> configs = new ArrayList<>();
-//     private GTParser parser;
-//     private final Timer timer = new Timer();
-//     private String data = null;
-//     private long lastRun = 0;
-//     private AmazonS3 client;
+// private Map<String, ServerData> servers = new HashMap<>();
+// private List<ServerConfig> configs = new ArrayList<>();
+// private GTParser parser;
+// private final Timer timer = new Timer();
+// private String data = null;
+// private long lastRun = 0;
+// private AmazonS3 client;
 
-//     public static void main(String[] args) {
-//         new JavaStats();
-//         SpringApplication.run(JavaStats.class, args);
-//     }
+// public static void main(String[] args) {
+// new JavaStats();
+// SpringApplication.run(JavaStats.class, args);
+// }
 
-//     public JavaStats() {
-//         try {
-//             client = AmazonS3ClientBuilder.standard()
-//                     .withCredentials(new DefaultAWSCredentialsProviderChain())
-//                     .withRegion(Regions.US_WEST_1).build();
-//         } catch (Exception e) {
-//             throw e;
-//         }
+// public JavaStats() {
+// try {
+// client = AmazonS3ClientBuilder.standard()
+// .withCredentials(new DefaultAWSCredentialsProviderChain())
+// .withRegion(Regions.US_WEST_1).build();
+// } catch (Exception e) {
+// throw e;
+// }
 
-//         configs.add(new ServerConfig("jb.csgo.edgegamers.cc:27015", "Jailbreak"));
-//         configs.add(new ServerConfig("ttt.csgo.edgegamers.cc:27015", "Trouble in Terrorist Town"));
-//         configs.add(new ServerConfig("ttt.csgo.edgegamers.cc:27015", "Surf"));
-//         configs.add(new ServerConfig("surf.csgo.edgegamers.cc:27015", "Surf Summer"));
-//         configs.add(new ServerConfig("mg.csgo.edgegamers.cc:27015", "Minigames"));
-//         configs.add(new ServerConfig("bhop.csgo.edgegamers.cc:27015", "Bhop"));
-//         configs.add(new ServerConfig("104.128.58.205:27015", "AWP Only"));
-//         configs.add(new ServerConfig("104.128.58.204:27015", "KZ Climb"));
+// configs.add(new ServerConfig("jb.csgo.edgegamers.cc:27015", "Jailbreak"));
+// configs.add(new ServerConfig("ttt.csgo.edgegamers.cc:27015", "Trouble in
+// Terrorist Town"));
+// configs.add(new ServerConfig("ttt.csgo.edgegamers.cc:27015", "Surf"));
+// configs.add(new ServerConfig("surf.csgo.edgegamers.cc:27015", "Surf
+// Summer"));
+// configs.add(new ServerConfig("mg.csgo.edgegamers.cc:27015", "Minigames"));
+// configs.add(new ServerConfig("bhop.csgo.edgegamers.cc:27015", "Bhop"));
+// configs.add(new ServerConfig("104.128.58.205:27015", "AWP Only"));
+// configs.add(new ServerConfig("104.128.58.204:27015", "KZ Climb"));
 
-//         configs.sort((a, b) -> a.getName().compareTo(b.getName()));
+// configs.sort((a, b) -> a.getName().compareTo(b.getName()));
 
-//         for (ServerConfig config : configs)
-//             servers.put(config.getName(), new AWSServerData(client, config));
+// for (ServerConfig config : configs)
+// servers.put(config.getName(), new AWSServerData(client, config));
 
-//         StatConfig statConfig = new StatConfig("https://www.gametracker.com/server_info/");
+// StatConfig statConfig = new
+// StatConfig("https://www.gametracker.com/server_info/");
 
-//         parser = new GTParser(statConfig);
-//         timer.schedule(this, 0, TimeUnit.HOURS.toMillis(12));
-//     }
+// parser = new GTParser(statConfig);
+// timer.schedule(this, 0, TimeUnit.HOURS.toMillis(12));
+// }
 
-//     @RequestMapping("/")
-//     @ResponseBody
-//     public String getGreeting() {
-//         run();
-//         return data;
-//     }
+// @RequestMapping("/")
+// @ResponseBody
+// public String getGreeting() {
+// run();
+// return data;
+// }
 
-//     @Override
-//     public void run() {
-//         if (System.currentTimeMillis() - lastRun < TimeUnit.HOURS.toMillis(1))
-//             return;
-//         lastRun = System.currentTimeMillis();
-//         for (ServerConfig config : configs) {
-//             ServerData data = servers.get(config.getName());
-//             data.addData(parser.parseData(config));
-//             data.save();
-//         }
-//         Formatter format = new ForumsFormat();
-//         System.out.println(format.format(servers.values()));
-//         data = format.format(servers.values()).replace(System.lineSeparator(), "<br>");
-//     }
+// @Override
+// public void run() {
+// if (System.currentTimeMillis() - lastRun < TimeUnit.HOURS.toMillis(1))
+// return;
+// lastRun = System.currentTimeMillis();
+// for (ServerConfig config : configs) {
+// ServerData data = servers.get(config.getName());
+// data.addData(parser.parseData(config));
+// data.save();
+// }
+// Formatter format = new ForumsFormat();
+// System.out.println(format.format(servers.values()));
+// data = format.format(servers.values()).replace(System.lineSeparator(),
+// "<br>");
+// }
 // }
